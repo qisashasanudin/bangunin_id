89f47d04485b23e394f014d59c9e19f8490e6202
Hanvey <hanveyxavero888@gmail.com>
2020-10-31 12:24:13 +0700

    icon app

--- lib/shared/decorations.dart
+++ lib/shared/decorations.dart
@@ -6,8 +6,7 @@ class AppColors {
   final accent1 = Colors.white;
   final accent2 = Colors.black;
   final accent3 = Colors.grey[300];
-
-  const AppColors();
+  AppColors();
 }
 
 SystemUiOverlayStyle transparentAppbarAndNavbar() {
