5d8cd192fb132ecf368d4aa3fd12d7b1fa299bac
qisashasanudin <qisas.tazkia@ui.ac.id>
2020-11-01 12:02:30 +0700

    user info berhasil ditampilkan di layar

--- lib/screens/mainmenu/tab_home.dart
+++ lib/screens/mainmenu/tab_home.dart
@@ -1,7 +1,5 @@
 import 'package:bangunin_id/models/user.dart';
 import 'package:bangunin_id/shared/decorations.dart'; // sumber AppColors()
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:bangunin_id/services/database.dart';
