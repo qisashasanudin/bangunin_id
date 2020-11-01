d8dddfb6949f294f370a3683d74ddeb2927b8983
Hanvey <hanveyxavero888@gmail.com>
2020-11-01 17:11:06 +0700

    tab_acc

--- lib/screens/mainmenu/tab_account.dart
+++ lib/screens/mainmenu/tab_account.dart
@@ -71,3 +71,20 @@ class Account extends StatelessWidget {
     );
   }
 }
+
+//Color(0xff555000)
+class HeaderCurvedContainer extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()..color = const Color(0xff555000);
+    Path path = Path()
+      ..relativeLineTo(0, 150)
+      ..quadraticBezierTo(size.width / 2, 250.0, size.width, 150)
+      ..relativeLineTo(0, -150)
+      ..close();
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
