0b166400b63e27c74188a68f579c3eae51917c49
Hanvey <hanveyxavero888@gmail.com>
2020-11-01 23:22:35 +0700

    cleaning code

--- lib/screens/mainmenu/tab_account.dart
+++ lib/screens/mainmenu/tab_account.dart
@@ -197,20 +197,3 @@ class HomeAppBar extends SliverPersistentHeaderDelegate {
     );
   }
 }
-
-//Color(0xff555000)
-class HeaderCurvedContainer extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    Paint paint = Paint()..color = const Color(0xff555000);
-    Path path = Path()
-      ..relativeLineTo(0, 150)
-      ..quadraticBezierTo(size.width / 2, 250.0, size.width, 150)
-      ..relativeLineTo(0, -150)
-      ..close();
-    canvas.drawPath(path, paint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) => false;
-}
