ef5c2077fb7fbbd6e8b06768b7587b0a9d68a693
Qisas Hasanudin <qisas.tazkia@ui.ac.id>
2020-11-28 13:08:18 +0700

    minor fixes

--- lib/shared/UI_components/app_colors.dart
+++ lib/shared/UI_components/app_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  final primary = const Color(0xffffc000);
+  final primary = Colors.amber;
   final accent1 = Colors.white;
   final accent2 = Colors.black;
   final accent3 = Colors.grey[300];
