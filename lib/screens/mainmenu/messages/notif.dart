3aec3d3d0c16b492f17667f67feb9243096d11ec
Hanvey <hanveyxavero888@gmail.com>
2020-12-05 01:16:07 +0700

    notification updates

--- lib/screens/mainmenu/messages/notif.dart
+++ lib/screens/mainmenu/messages/notif.dart
@@ -17,17 +17,17 @@ class _NotifState extends State<Notif> {
             CustomHeading(
               title: 'Tracking Orders',
             ),
-            _trackingOrder('Cat Merah', 'Rp. 150.000,00'),
-            _trackingOrder('Pintu', 'Rp. 10.000,00'),
-            _trackingOrder('Lemari', 'Rp. 3.000.000,00'),
-            _trackingOrder('Batu Bata', 'Rp. 10.000,00'),
+            _trackingOrder('Cat Merah', 'Rp. 150.000,00', 'Deliver'),
+            _trackingOrder('Pintu', 'Rp. 10.000,00', 'Deliver'),
+            _trackingOrder('Lemari', 'Rp. 3.000.000,00', 'Arrived'),
+            _trackingOrder('Batu Bata', 'Rp. 10.000,00', 'Arrived'),
           ],
         ),
       ),
     );
   }
 
-  Container _trackingOrder(String judul, String harga) {
+  Container _trackingOrder(String judul, String harga, String deliver) {
     return Container(
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 5),
@@ -45,7 +45,7 @@ class _NotifState extends State<Notif> {
                   ),
                 ),
                 Text(
-                  'Delivering',
+                  deliver,
                   style: TextStyle(
                     color: Colors.greenAccent,
                     fontWeight: FontWeight.normal,
