bac8690b5d0c95e21fbfcdc651c7c181552dd0b7
Hanvey <hanveyxavero888@gmail.com>
2020-12-08 15:12:28 +0700

    save

--- lib/screens/mainmenu/shop/tab_checkout.dart
+++ lib/screens/mainmenu/shop/tab_checkout.dart
@@ -18,10 +18,21 @@ class _CheckoutTabState extends State<CheckoutTab> {
         title: Text('Checkout'),
       ),
       body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 10.0),
         child: Column(
           children: [
-            CustomHeading(
-              title: 'Your Order',
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                CustomHeading(
+                  title: 'Your Order',
+                ),
+                Text(
+                  'Add Items + ',
+                  style: TextStyle(
+                      color: Colors.orangeAccent, fontWeight: FontWeight.bold),
+                ),
+              ],
             ),
             // widget-widget lain dimasukkan di sini
           ],
@@ -30,6 +41,66 @@ class _CheckoutTabState extends State<CheckoutTab> {
     );
   }
   //========================= main function =========================
+
+  Container _trackingOrder(String judul, String harga, String deliver) {
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 5),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                new Text(
+                  judul,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 18,
+                  ),
+                ),
+                Text(
+                  deliver,
+                  style: TextStyle(
+                    color: Colors.greenAccent,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  new Text(
+                    '5 Desember 2020 \n5 pail',
+                    style: TextStyle(
+                      color: Colors.black38,
+                      fontWeight: FontWeight.normal,
+                      fontSize: 14,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                  Text(
+                    harga,
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontWeight: FontWeight.normal,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Divider(),
+          ],
+        ),
+      ),
+    );
+  }
 
   ListTile language() {
     return ListTile(
